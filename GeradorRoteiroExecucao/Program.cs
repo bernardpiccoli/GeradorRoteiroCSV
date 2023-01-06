971f3df7f3ab8ce52eb33d33ff0a8931dfd1bb51
Bernard Piccoli <bernard.piccoli@outlook.com>
2023-01-06 02:43:21 -0300

    Add files via upload

---
+++ GeradorRoteiroExecucao/Program.cs
@@ -0,0 +1,76 @@
+﻿using GeradorRoteiroExecucao;
+using System.Configuration;
+
+Console.WriteLine("O Aquivo será montado de acordo com a ordem alfabetica dos aquivos. Coloque-os na pasta script");
+Console.WriteLine("Ficará no formato: (1, 001_SCRIPT.SQL, host, schema). Pressione ok para confirmar!");
+Console.WriteLine("Pressione ENTER para confirmar!");
+Console.ReadKey();
+
+Console.WriteLine("Digite o nome do host");
+var host = Console.ReadLine();
+if (string.IsNullOrEmpty(host)) return;
+
+Console.WriteLine("Digite o nome do schema");
+var schema = Console.ReadLine();
+if (string.IsNullOrEmpty(schema)) return;
+
+var folder = ConfigurationManager.AppSettings["folder"];
+if (string.IsNullOrEmpty(folder)) { Console.WriteLine("Variável folder não declarada na App.config."); return; };
+
+var arquivoCSV = ConfigurationManager.AppSettings["arquivoCSV"];
+if (string.IsNullOrEmpty(arquivoCSV)) { Console.WriteLine("Variável folder não declarada na App.config."); return; } ;
+
+var arquivosEncontrados = RetornaNomeArquivos(AppDomain.CurrentDomain.BaseDirectory + "\\" + folder);
+
+if (arquivosEncontrados.Count <= 0)
+    Console.WriteLine("A pasta não possui arquivos.");
+else
+    EscreverLinhasCSV(MontarLinhasObjeto(arquivosEncontrados, host, schema), folder, arquivoCSV);
+//arquivosEncontrados.ForEach(delegate(string nome) { Console.WriteLine(nome); });
+
+Console.WriteLine("Concluído com sucesso!");
+Console.ReadKey();
+
+static List<string> RetornaNomeArquivos(string caminho)
+{
+    //Também pega informações de um arquivo passado o path:
+    //FileInfo infoArquivo = new FileInfo("E:\\NFE.xml");
+    //infoArquivo.Name
+
+    if (Directory.Exists(caminho) && !string.IsNullOrEmpty(caminho))
+        return Directory.GetFiles(caminho).ToList().Select(x => { x = Path.GetFileName(x); return x; }).ToList();
+    //Directory.GetFiles(caminho).ToList().Select(x => Path.GetFileName(x)).ToList();
+    else
+        return new List<string>();
+}
+
+static List<Roteiro> MontarLinhasObjeto(List<string> nomeArquivos, string host, string schema)
+{
+    List<Roteiro> roteiros = new List<Roteiro>();
+
+    foreach (var nomeArquivo in nomeArquivos)
+    {
+        var roteiro = new Roteiro
+        {
+            Id = nomeArquivos.IndexOf(nomeArquivo)+1,
+            Arquivo = nomeArquivo,
+            Schema = schema,
+            Host = host
+        };
+        roteiros.Add(roteiro);
+    }
+    return roteiros;
+}
+
+static void EscreverLinhasCSV(List<Roteiro> roteiros, string folder, string arquivoCSV)
+{
+
+    if (File.Exists(AppDomain.CurrentDomain.BaseDirectory + "\\" + folder + "\\" + arquivoCSV))
+        Console.WriteLine("Já existe um arquivo de roteiro na pasta. Delete-o antes de executar.");
+
+    else // temos duas formas de converter pra string ( veja na classe Roteiro )
+        //File.WriteAllLines(AppDomain.CurrentDomain.BaseDirectory + "\\" + folder + "\\" + arquivoCSV, roteiros.Select(roteiro => roteiro.ToString()).ToList());
+        File.WriteAllLines(AppDomain.CurrentDomain.BaseDirectory + "\\" + folder + "\\" + arquivoCSV, roteiros.Select(roteiro => (string)roteiro).ToList());
+}
+
+
---
+++ GeradorRoteiroExecucao/Roteiro.cs
@@ -0,0 +1,33 @@
+﻿using System.Numerics;
+
+namespace GeradorRoteiroExecucao
+{
+    public class Roteiro
+    {
+        public int Id { get; set; }
+        public string? Arquivo { get; set; }
+        public string? Host { get; set; }
+        public string? Schema { get; set; }
+
+        //sobrescrevendo o método to string, que herda de System
+        //public override string ToString()
+        //{
+        //    return $"{Id}, {Arquivo}, {Host}, {Schema}";
+
+        //    ////object
+        //    //var roteiro = new Roteiro("1", "abc.csv", "HOST", "SCHEMA");
+        //    //Console.WriteLine(roteiro);
+
+        //    ////string
+        //    //var rot = "1 abc.csv host schema";
+        //    //roteiro = rot;
+
+        //    //Console.WriteLine(rot);
+        //}
+
+        public static implicit operator string(Roteiro roteiro) => $"{roteiro.Id},{roteiro.Arquivo},{roteiro.Host},{roteiro.Schema}";
+
+    }
+
+
+}
